ea27617bb4c53c5aba91a6a69632b9886fe50427
llaoj <qustwwy@163.com>
2022-08-16 17:02:11 +0800

    mkdir

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-wget -O kubespray.tar.gz https://github.com/kubernetes-sigs/kubespray/archive/refs/tags/v2.18.0.tar.gz
-tar -zxvf kubespray.tar.gz -C kubespray-2.18.0
+wget -O kubespray-2.18.0.tar.gz https://github.com/kubernetes-sigs/kubespray/archive/refs/tags/v2.18.0.tar.gz
+mkdir kubespray-2.18.0
+tar -zxvf kubespray-2.18.0.tar.gz -C kubespray-2.18.0
 
 cd kubespray-2.18.0/contrib/offline
 bash generate_list.sh
