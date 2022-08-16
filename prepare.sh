4d2ecca219e68a438e73e1a346bbda648af2bd4d
llaoj <qustwwy@163.com>
2022-08-16 17:20:54 +0800

    2.18.1

--- prepare.sh
+++ prepare.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# wget -O kubespray-2.18.0.tar.gz https://github.com/kubernetes-sigs/kubespray/archive/refs/tags/v2.18.0.tar.gz
-# mkdir kubespray-2.18.0
-# tar -zxvf kubespray-2.18.0.tar.gz -C kubespray-2.18.0
+wget https://github.com/kubernetes-sigs/kubespray/archive/refs/tags/v2.18.1.zip
+unzip v2.18.1.zip
 
-wget https://github.com/kubernetes-sigs/kubespray/archive/refs/tags/v2.18.0.zip
-unzip v2.18.0.zip
-
-cd kubespray-2.18.0/contrib/offline
+cd kubespray-2.18.1/contrib/offline
 bash generate_list.sh
 tree temp/
 
