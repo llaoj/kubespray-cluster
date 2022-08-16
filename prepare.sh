b4e88c0a3b705c1c6e1b658c8dfd1ebe6ba6066e
llaoj <qustwwy@163.com>
2022-08-16 17:56:07 +0800

    1.22.5

--- prepare.sh
+++ prepare.sh
@@ -6,7 +6,7 @@ unzip v2.18.1.zip
 cp -r inventory/mycluster kubespray-2.18.1/inventory/
 cd kubespray-2.18.1/contrib/offline
 
-./generate_list.sh -i /inventory/mycluster/inventory.ini
+./generate_list.sh -i inventory/mycluster/inventory.ini
 tree temp/
 cat temp/files.list
 cat temp/images.list
