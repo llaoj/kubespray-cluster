7c89e6b57be3b2c0170e4b671347c060c6b2660f
llaoj <qustwwy@163.com>
2022-08-16 18:22:25 +0800

    download files

--- prepare.sh
+++ prepare.sh
@@ -5,10 +5,15 @@ unzip -q v2.18.1.zip
 
 cp -r inventory/mycluster kubespray-2.18.1/inventory/
 cd kubespray-2.18.1/contrib/offline
-
 ./generate_list.sh -i inventory/mycluster/inventory.ini
-tree temp/
 cat temp/files.list
 cat temp/images.list
+
+wget -x -P temp/files -i temp/files.list
+tree temp/
+
+wget https://gosspublic.alicdn.com/ossutil/1.7.13/ossutil64
+chmod 755 ossutil64
+# ./ossutil64 cp localfolder/ oss://examplebucket/desfolder/ -r
 
 
