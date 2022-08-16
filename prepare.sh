288ce7ed975170b0f73c8f4609b2dd9a1d2f21f8
llaoj <qustwwy@163.com>
2022-08-16 17:10:07 +0800

    v2.18.0.zip

--- prepare.sh
+++ prepare.sh
@@ -5,7 +5,7 @@
 # tar -zxvf kubespray-2.18.0.tar.gz -C kubespray-2.18.0
 
 wget https://github.com/kubernetes-sigs/kubespray/archive/refs/tags/v2.18.0.zip
-unzip kubespray-2.18.0.zip
+unzip v2.18.0.zip
 
 cd kubespray-2.18.0/contrib/offline
 bash generate_list.sh
