a7bdc00f36b9bbdb112048cf37e1208a7ede534a
llaoj <qustwwy@163.com>
2022-08-16 17:46:43 +0800

    offline

--- prepare.sh
+++ prepare.sh
@@ -3,9 +3,9 @@
 wget https://github.com/kubernetes-sigs/kubespray/archive/refs/tags/v2.18.1.zip
 unzip v2.18.1.zip
 
-cd kubespray-2.18.1/contrib/offline
-bash generate_list.sh
+./kubespray-2.18.1/contrib/offline/generate_list.sh -i inventory/mycluster/inventory.ini
 tree temp/
-
+cat temp/files.list
+cat temp/images.list
 
 
