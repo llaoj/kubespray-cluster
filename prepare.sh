3a9dc35766b279d65a73df6ca59fce15d3efe049
llaoj <qustwwy@163.com>
2022-08-16 18:01:18 +0800

    quite

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wget https://github.com/kubernetes-sigs/kubespray/archive/refs/tags/v2.18.1.zip
-unzip v2.18.1.zip
+unzip -q v2.18.1.zip
 
 cp -r inventory/mycluster kubespray-2.18.1/inventory/
 cd kubespray-2.18.1/contrib/offline
