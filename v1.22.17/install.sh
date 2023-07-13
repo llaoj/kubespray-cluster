099906852033772d95f6cc2172256c6dda061c59
llaoj <qustwwy@163.com>
2023-07-13 10:36:53 +0800

    v1.22.17

---
+++ v1.22.17/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+
+KUBSPRAY_VERSION=v2.18.2
+
+wget -O kubespray-src.zip https://github.com/kubernetes-sigs/kubespray/archive/refs/tags/${KUBSPRAY_VERSION}.zip
+unzip -q kubespray-src.zip
+SRC_PATH=$(unzip -l kubespray-src.zip | sed -n '5p' | awk '{print $4}')
+
+cp -rf inventroy/mycluster "${SRC_PATH}"inventory/
+cd "${SRC_PATH}" || return
+ansible-playbook -i inventory/mycluster/inventory.ini cluster.yml
