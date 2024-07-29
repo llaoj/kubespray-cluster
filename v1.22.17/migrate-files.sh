d373e66b81ab6190157770588ee0af5da2752d38
老J <hmmmbiubiubiu@gmail.com>
2024-07-29 13:36:01 +0800

    oss

--- v1.22.17/migrate-files.sh
+++ v1.22.17/migrate-files.sh
@@ -6,8 +6,9 @@ OSS_ENDPOINT=oss-cn-beijing.aliyuncs.com
 OSS_CLOUD_URL=llaoj/kubernetes/
 
 # echo "Download files and upload to OSS"
+mkdir -p /tmp/files
 wget -qx -P /tmp/files -i ./files.txt
-tree temp/
+tree /tmp/files
 wget -q https://gosspublic.alicdn.com/ossutil/1.7.13/ossutil64 && chmod 755 ossutil64
 ./ossutil64 \
   -e $OSS_ENDPOINT \
